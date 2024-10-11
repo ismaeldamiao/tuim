387bfa54fb064e3f6bdf7f0258b89156489b2417
ismaeldamiao <ismaellxd@gmail.com>
2024-10-11 11:06:46 -0300

    Minor improvements on CLI.

--- src-cli/tuim.c
+++ src-cli/tuim.c
@@ -1,17 +1,35 @@
-/*
-cmake -S tuim -B tuim/build
-cmake --build tuim/build
-cc -I../src-loader -o tuim tuim.c -L ../src-loader/build -ltuim
-*/
+/* *****************************************************************************
+   MIT License
 
-// Front-end to use tuim from the command line interface
+   Copyright (c) 2024 I.F.F. dos Santos <ismaellxd@gmail.com>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the “Software”), to
+   deal in the Software without restriction, including without limitation the
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+   sell copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+***************************************************************************** */
 #include <stdio.h>
 #include <stdlib.h>
 #include <tuim.h>
 
-//#include "crt/crt.h"
-
-unsigned int status;
+/* ------------------------------------
+   Interface for Tuim's ELF loader, to execute program from command line.
+   * Part of tuim project.
+   * Last modified: Octubre 11, 2024.
+------------------------------------ */
 
 int main(int argc, char **argv){
    tuim_elf *elf;
@@ -46,12 +64,9 @@ int main(int argc, char **argv){
 
    main_ = tuim_getentry(elf);
    if(main_){
-      //main_ = crt(main_, --argc, ++argv, &return_code);
-      return_code = EXIT_SUCCESS;
-      //main_();
-      return_code = ((int(*)(void))main_)();
+      return_code = ((int(*)(int,char**))main_)(--argc, ++argv);
    }else{
-      fputs("Can't find the 'main' function.\n", stderr);
+      fputs("Can't find the entry point.\n", stderr);
       return_code = EXIT_FAILURE;
    }
 
