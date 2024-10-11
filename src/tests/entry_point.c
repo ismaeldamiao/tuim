9c8538bc5908f7583f3943d0b2c3e87fd6935ecf
ismaeldamiao <ismaellxd@gmail.com>
2024-10-11 11:08:24 -0300

    Removed some files...

--- src/tests/entry_point.c
+++
@@ -1,6 +0,0 @@
-// cc -fPIE -o entry_point entry_point.c
-#include <stdio.h>
-
-int main(void){
-    printf("main is at %p\n", &main);
-}
--- src/tests/ola.c
+++
@@ -1,6 +0,0 @@
-// cc -fPIE -o ola ola.c
-#include <stdio.h>
-
-int main(void){
-    puts("Ola Mundo!");
-}
