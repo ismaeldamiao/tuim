d85748687e8bc02feccc5b3753083209cd63d73a
ismaeldamiao <ismaellxd@gmail.com>
2024-10-15 10:56:50 -0300

    Batch test 1

--- scripts/build.bat
+++ scripts/build.bat
@@ -14,7 +14,7 @@ echo Passou aqui 0!
 cl.exe ^
    /c ^
    /DTUIM_HOME="%TUIM_HOME%" ^
-   /Fo.build/libtuim.obj ^
+   /Fo.build\libtuim.obj ^
    src-loader\libtuim.c
 if %errorlevel% NEQ 0 (
    exit /b %errorlevel%
@@ -24,7 +24,7 @@ echo Passou aqui 1!
 
 cl.exe ^
    /I src-loader\include ^
-   /Fe.build/tuim.exe ^
+   /Fe.build\tuim.exe ^
    src-cli\tuim.c .build\libtuim.obj
 
 echo Passou aqui 2!
