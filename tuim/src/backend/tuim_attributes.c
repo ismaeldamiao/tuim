bdb073cb2ea93974dd72411893764e63ec23eedb
ismaeldamiao <ismaellxd@gmail.com>
2025-07-11 09:36:25 -0300

    Try build with workflow 9

--- tuim/src/backend/tuim_attributes.c
+++ tuim/src/backend/tuim_attributes.c
@@ -4,6 +4,8 @@
    #error Not implemented yet
 #elif defined(TUIM_BUILD_AARCH32)
    #include "armel/tuim_attributes.c"
+#elif defined(TUIM_BUILD_AARCH64)
+   #include "aarch64/tuim_attributes.c"
 #elif defined(TUIM_BUILD_AMD64)
    #include "amd64/tuim_attributes.c"
 #endif
--- tuim/src/backend/tuim_relocate.c
+++ tuim/src/backend/tuim_relocate.c
@@ -1,9 +1,11 @@
-#if defined(__ARM_ARCH)
-   #if __ARM_ARCH <= 7
-      #include "armel/tuim_relocate.c"
-   #elif __ARM_ARCH > 7
-      #include "aarch64/tuim_relocate.c"
-   #endif
-#else
-   #error Unknown target architecture
+#if defined(TUIM_BUILD_RISCV32)
+   #error Not implemented yet
+#elif defined(TUIM_BUILD_RISCV64)
+   #error Not implemented yet
+#elif defined(TUIM_BUILD_AARCH32)
+   #include "armel/tuim_relocate.c"
+#elif defined(TUIM_BUILD_AARCH64)
+   #include "aarch64/tuim_relocate.c"
+#elif defined(TUIM_BUILD_AMD64)
+   #include "amd64/tuim_relocate.c"
 #endif
